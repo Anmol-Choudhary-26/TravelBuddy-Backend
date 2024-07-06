cf7a02d6c95dd9657f43c04bdc36b96d196b34d6
Anmol Choudhary <ac038960@gmail.com>
2024-07-06 11:28:33 +0530

    chatRooms created

--- server.js
+++ server.js
@@ -8,7 +8,11 @@ import chatRouter from './controllers/chatControllers.js';
 import dotenv from 'dotenv'
 dotenv.config()
 import cors from 'cors'
-
+const corsOptions = {
+    origin: 'https://travelbudddyy.netlify.app',
+    credentials: true,
+    optionsSuccessStatus: 200,
+}
 // connected to database
 const client = new MongoClient(
     process.env.MONGO_URL,
@@ -22,11 +26,12 @@ const client = new MongoClient(
 );
 
 // middleware
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/public', express.static('public'));
 app.use('/public', serveIndex('public'));
+
 
 // Routes
 app.use("/user", usersRouter);
