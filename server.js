f44d825055227759a8032f95a06c3fcb9768fd19
Anmol Choudhary <ac038960@gmail.com>
2024-07-06 13:11:54 +0530

    cors settings updated

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ const client = new MongoClient(
 );
 
 // middleware
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://travelbudy.netlify.app');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+  });
 app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
