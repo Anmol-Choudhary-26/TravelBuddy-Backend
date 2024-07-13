4ccc26880975a5165a0c63108e6a667c666be9fc
Anmol Choudhary <ac038960@gmail.com>
2024-07-13 17:30:57 +0530

    bug fixes

--- controllers/bookmarkControllers.js
+++ controllers/bookmarkControllers.js
@@ -39,7 +39,7 @@ router.get('/getbookmark', async (req, res) => {
     res.status(200).json(posts);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ error });
   }
 });
 
