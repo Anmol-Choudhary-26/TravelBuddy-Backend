5b52a1412d21b87ace27dd3528243ee3332ea45d
Anmol Choudhary <ac038960@gmail.com>
2024-08-02 20:50:31 +0530

    updated chat

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -1,39 +1,72 @@
-import express from 'express';
+import express from "express";
 const router = express.Router();
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 // Create a new chat room
-router.post('/',  async (req, res) => {
-  const { users } = req.body;
-  const chatRoom = await prisma.chatRoom.create({
-    data: {
-      users: {
-        create: users.map((userId) => ({
-          user: {
-            connect: { id: userId },
-          },
-        })),
+router.post("/", async (req, res) => {
+  try {
+    const { firstId, secondId } = req.body;
+
+    const chat = await prisma.chatRoom.findFirst({
+      where: {
+        OR: [
+          { AND: [{ user1: firstId }, { user2: secondId }] },
+          { AND: [{ user1: secondId }, { user2: firstId }] },
+        ],
       },
-    },
-  });
+    });
+    if (chat) return res.status(200).json(chat);
 
-  res.status(200).json(chatRoom);
-}
+    const chatRoom = await prisma.chatRoom.create({
+      data: {
+        user1: firstId,
+        user2: secondId,
+      },
+    });
 
-)
-// Send a message to a chat room
-router.post('/msg', async (chatRoomId, userId, message) => {
-  const msg = await prisma.msg.create({
-    data: {
-      message,
-      chatRoomId,
-      userId,
-    },
-  });
+    res.status(200).json(chatRoom);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
-  res.status(200).json(msg);
-}
-)
+router.get("/getchats", async (req, res) => {
+  try {
+    const { userId } = req.query;
+    console.log(userId)
+
+    const chatRooms = await prisma.chatRoom.findMany({
+      where: {
+        OR: [{ user1: userId }, { user2: userId }],
+      },
+    });
+
+    res.status(200).json(chatRooms);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
+router.get('/singlechat', async (req, res) => {
+  try{
+    const { firstId, secondId } = req.body;
+
+    const chat = await prisma.chatRoom.findFirst({
+      where: {
+        OR: [
+          { AND: [{ user1: firstId }, { user2: secondId }] },
+          { AND: [{ user1: secondId }, { user2: firstId }] },
+        ],
+      },
+    });
+    if (chat) return res.status(200).json(chat);
+  }
+  catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+})
 
 export default router;
